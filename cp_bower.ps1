02860e89c6e68f52b0d554ecafc18f43dabcdba6
chidea <sbw228@gmail.com>
2016-06-04 01:48:25 +0900

    Update cp_bower.ps1

--- cp_bower.ps1
+++ cp_bower.ps1
@@ -5,12 +5,12 @@ pushd
 cd app/src/main/assets/www
 rm */test -force -recurse
 rm */demo -force -recurse
-rm *\CONTRIBUTING.md
-rm *\README.md
-rm *\.travis.yml
-rm *\bower.json
-rm *\.bower.json
-rm *\.gitignore
-rm *\index.html
+rm */CONTRIBUTING.md
+rm */README.md
+rm */.travis.yml
+rm */bower.json
+rm */.bower.json
+rm */.gitignore
+rm */index.html
 
 popd
